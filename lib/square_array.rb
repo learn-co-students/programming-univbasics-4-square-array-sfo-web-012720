578271fd74f880633c3d22be0ae9a31f75488c28
Austin Osorio <osorio_austin@yahoo.com>
2019-12-26 07:47:29 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,11 @@
 def square_array(array)
   count = 0
+  new_array = []
 
   while array[count] do
-    p "test"
-    count++
-  end
+    new_array << array[count]**2
+    count += 1
 
+  end
+  new_array
 end
